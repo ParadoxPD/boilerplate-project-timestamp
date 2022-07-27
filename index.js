6f1c4efa11931c4520b2f47165b7f769074a0633
ParadoxPD <ask.priyanshu@gmail.com>
2022-07-27 13:23:34 +0530

    Done

--- index.js
+++ index.js
@@ -14,10 +14,28 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get("/api", (req, res) => {
+  const now = new Date();
+  res.json({
+    unix: now.getTime(),
+    utc: now.toUTCString()
+  });
+})
 app.get("/api/:timestamp", (req, res) => {
-  const timestamp = req.params.timestamp;
-  res.send("HEllo");
-  console.log(timestamp);
+  const paramsDate = req.params.timestamp;
+  const invalidDate = "Invalid Date";
+  const date = parseInt(paramsDate) < 10000 ?
+    new Date(paramsDate) :
+    new Date(parseInt(paramsDate))
+
+  date.toString() === invalidDate ?
+    res.json({
+      error: invalidDate
+    }) :
+    res.json({
+      unix: date.valueOf(),
+      utc: date.toUTCString()
+    });
 })
 
 
@@ -31,6 +49,6 @@ app.get("/api/hello", function (req, res) {
 
 
 // listen for requests :)
-const listener = app.listen(process.env.PORT, function () {
+const listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
